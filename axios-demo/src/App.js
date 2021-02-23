cc838d5cf75688b2d918aaaa08f74e736ad7c060
Kavita Gupta <kavitagupta972@gmail.com>
2021-02-23 14:51:22 +0530

    react table with axios crud

---
+++ axios-demo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ReactTableComponent from './components/ReactTableComponent';
+
+function App() {
+  return (
+    <React.Fragment>
+
+      <ReactTableComponent />
+    </React.Fragment>
+  );
+}
+
+export default App;
